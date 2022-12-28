3ea55e9110d922732c263cbb21fc4a21a4383623
MinhDuyDEV <94704296+MinhDuyDEV@users.noreply.github.com>
2022-12-28 09:59:06 +0700

    optimize import

--- src/App.js
+++ src/App.js
@@ -1,33 +1,35 @@
-import React, { Fragment } from "react";
+import React, { Fragment, lazy, Suspense } from "react";
 import "swiper/scss";
 import { Route, Routes } from "react-router-dom";
 import Main from "./components/layout/Main";
-import HomePage from "./pages/HomePage";
 import Banner from "./components/banner/Banner";
-import MoviePage from "./pages/MoviePage";
-import MovieDetailsPage from "./pages/MovieDetailsPage";
+const HomePage = lazy(() => import("./pages/HomePage"));
+const MoviePage = lazy(() => import("./pages/MoviePage"));
+const MovieDetailsPage = lazy(() => import("./pages/MovieDetailsPage"));
 
 const App = () => {
   return (
     <Fragment>
-      <Routes>
-        <Route element={<Main></Main>}>
-          <Route
-            path="/"
-            element={
-              <>
-                <Banner></Banner>
-                <HomePage></HomePage>
-              </>
-            }
-          ></Route>
-          <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
-          <Route
-            path="/movies/:movieId"
-            element={<MovieDetailsPage></MovieDetailsPage>}
-          ></Route>
-        </Route>
-      </Routes>
+      <Suspense fallback={<></>}>
+        <Routes>
+          <Route element={<Main></Main>}>
+            <Route
+              path="/"
+              element={
+                <>
+                  <Banner></Banner>
+                  <HomePage></HomePage>
+                </>
+              }
+            ></Route>
+            <Route path="/movies" element={<MoviePage></MoviePage>}></Route>
+            <Route
+              path="/movies/:movieId"
+              element={<MovieDetailsPage></MovieDetailsPage>}
+            ></Route>
+          </Route>
+        </Routes>
+      </Suspense>
     </Fragment>
   );
 };
